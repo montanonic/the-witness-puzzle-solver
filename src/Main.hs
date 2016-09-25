0a9b61b7f892cfe4c7aeb40f3f55df47151b2817
Nicholas Montaño <montanonic@gmail.com>
2016-09-25 15:23:21 -0700

    Neuter Main for now. GHCI is preferable for testing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,28 @@
 module Main where
 
+{-
 import Tetris
+import Point (Point(..))
+import qualified Point
 
-piece =
-    TetrisPiece { origin = Just (Point { x = 0, y = 0 })
-                , offsets = [ PointOffset { xOffset = id, yOffset = id }
-                            , PointOffset { xOffset = (+ 1), yOffset = id }
-                            , PointOffset { xOffset = (+ 2), yOffset = id }
-                            , PointOffset { xOffset = (+ 3), yOffset = id }
-                            ]
-                }
+piece = TetrisPiece
+    { origin = Just (Point { x = 0, y = 0 })
+    , offsets =
+        [ Point.Offset
+            { Point.xOffset = id
+            , Point.yOffset = id }
+        , Point.Offset
+            { Point.xOffset = (+ 1)
+            , Point.yOffset = id }
+        , Point.Offset
+            { Point.xOffset = (+ 2)
+            , Point.yOffset = id }
+        , Point.Offset
+            { Point.xOffset = (+ 3)
+            , Point.yOffset = id }
+        ]
+    }
+-}
 
 main :: IO ()
 main = do
